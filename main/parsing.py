cb836c08052a93831a67a375bf0364b79d3061a4
Agcon <90468059+Agcon@users.noreply.github.com>
2023-05-25 17:38:32 +0300

    version 1.0

---
+++ main/parsing.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import fake_useragent
+from txt_to_mp3 import *
+from config import *
+
+
+def avidreaders_parser(user_search): # парсинг сайта авидридерс
+    pass
+
+
+def tululu_parser(user_search): # парсинг сайта тулулу (много новых книг)
+    session = requests.Session()
+    user = fake_useragent.UserAgent().random
+    header = {'user-agent': user}
+
+    us = user_search.strip().replace(" ", "+").lower() # форматирование запроса
+    search_url = 'https://tululu.org/search/?q=' + us # поисковая ссылка 
+    responce = requests.get(search_url, headers=header).content
+    soup = bs(responce, "html.parser") # исходный код
+
+    # создание списка тегов с названием книги и ссылкой внутри
+    books = list()
+    for li in soup.find_all("li"):
+        h = li.find('h3')
+        if h:
+            books.append(h)
+        
+    #---------- test output ------------
+    print('Список найденных книг:')
+    [print(i) for i in books]
+    #-----------------------------------
+
+    if len(books) > 5:
+        pass # нужно конкретизировать поиск, т.к. слишком много результатов
+
+    books_names = [book.text for book in books] # список названий книг
+    return books_names, books
+
+
+def get_mp3(book_name, books_names, books):
+    book_url = "tululu.org" + books[books_names.index(book_name)].find('a').get('href')
+
+    # ссылка на скачивание txt
+    download_url = 'https://tululu.org/txt.php?id=' + book_url.split('/b')[1].replace('/', '')
+    # скачивание txt
+    response = requests.get(download_url)
+    book_dir = "{}/{}.txt".format(lib_path, book_name.replace(" ", ""))
+    open(book_dir, "wb").write(response.content)
+    txt_to_mp3_offline(book_dir)
+    return book_dir.replace(".txt", ".mp3")
+    
+
+if __name__ == "__main__":
+    tululu_parser("Бархатный сезон брэдбери")
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from pkg_resources import parse_requirements
+
+
+with open('requirements.txt') as f:
+    requirements = [str(req) for req in parse_requirements(f)]
+
+setup(
+    name='AudioBookBot',
+    version='1.0.0',
+    author='Agcon, pr0maxxx, MrGreys0n',
+    description='Converts text from books into audio',
+    packages=['main'],
+    install_requires=requirements,
+)
